3da45d8e8d7d08312e40f8ec21f6c0581e83811c
Federico Cáceres <federicoenmanuel@gmail.com>
2022-10-20 21:16:16 -0300

    Se agrega control para mostrar/ocultar mensajes en modal

--- js/logica.js
+++ js/logica.js
@@ -1,88 +1,41 @@
-var data = [
-            {
-                "estudiante":"María Patricia Heimburg Torres",
-                "ci":4883039,
-                codigo:1,
-                "curso":"Curso Básico de HTML5"
-            },
-            {
-                "estudiante":"Matías Nicolás Bao Cassanello",
-                "ci":5403489,
-                "codigo":2,
-                "curso":"Curso Básico de HTML5"
-            },
-            {
-                "estudiante":"Guido Fernando Alcaraz Herebia",
-                "ci":2837909,
-                "codigo":3,
-                "curso":"Curso Básico de HTML5"
-            },
-            {
-                "estudiante":"Laura Cecilia Achón Gómez",
-                "ci":3965412,
-                "codigo":4,
-                "curso":"Curso Básico de HTML5"
-            },
-            {
-                "estudiante":"Mariel Raquel Aquino Villalba",
-                "ci":3494074,
-                "codigo":5,
-                "curso":"Curso Básico de HTML5"
-            },
-            {
-                "estudiante":"Giovanni Arzamendia",
-                "ci":2551888,
-                "codigo":6,
-                "curso":"Curso Básico de HTML5"
-            },
-            {
-                "estudiante":"Roberto Fleitas",
-                "ci":3553821,
-                "codigo":7,
-                "curso":"Curso Básico de HTML5"
-            },
-            {
-                "estudiante":"Nilda Natalia Rigoni Ibañez",
-                "ci":3831278,
-                "codigo":8,
-                "curso":"Curso Básico de HTML5"
-            },
-            {
-                "estudiante":"Jorge Gustavo Flecha Nuñez",
-                "ci":2353583,
-                "codigo":9,
-                "curso":"Curso Básico de HTML5"
-            },
-            {
-                "estudiante":"Alejandro Milciades Flores Vera",
-                "ci":5718095,
-                "codigo":10,
-                "curso":"Curso Básico de HTML5"
-            }
-            ];
+
 
 function buscarAlumno(){
 
     codigo = document.getElementById("codigo").value;
+    msgOk = document.getElementById("certificadoEncontrado");
+    msgError = document.getElementById("certificadoNoEncontrado");
 
-    var found = data.find(e => e.codigo == codigo);
-    console.log(found);
-    if (found != undefined) {
-        var nombreEstudiante = document.getElementById("alumno");
-        var div = document.createElement("div");
+    eliminarDatosModal();
+    found = data.find(e => e.codigo == codigo);
+
+    if (found != undefined) 
+    {
+        nombreEstudiante = document.getElementById("alumno");
+        div = document.createElement("div");
         div.innerHTML = 
-        'Alumno: ' + found.estudiante + '<br>' + 
-        'C.I.: ' + found.ci + '<br>' + 
-        'Curso: ' + found.curso;
+        '<br>' +
+        '<strong>Alumno: </strong>' + found.estudiante + '<br>' + 
+        '<strong>C.I.: </strong>' + found.ci + '<br>' + 
+        '<strong>Curso: </strong>' + found.curso;
         nombreEstudiante.appendChild(div);
+        msgOk.style.display = "block";
+    }else{
+        msgError.style.display = "block";
     }
 
 }
 
-function eliminarDatosModal(){
-    var nombreEstudiante = document.getElementById("alumno");
-        
-    var child = nombreEstudiante.lastElementChild; 
+function eliminarDatosModal()
+{
+    nombreEstudiante = document.getElementById("alumno");
+    msgOk = document.getElementById("certificadoEncontrado");
+    msgError = document.getElementById("certificadoNoEncontrado");
+       
+    msgOk.style.display = "none";
+    msgError.style.display = "none";
+    
+    child = nombreEstudiante.lastElementChild; 
     while (child) {
         nombreEstudiante.removeChild(child);
         child = nombreEstudiante.lastElementChild;
@@ -90,13 +43,65 @@ function eliminarDatosModal(){
 }
 
 
-
-// const modal = document.getElementById("exampleModal");
-
-// document.addEventListener("click", (event) => {
-//   const isClickInside = modal.contains(event.target);
-
-//   if (!isClickInside) {
-//     eliminarDatosModal();
-//   }
-// });
+var data = [
+    {
+        "estudiante":"María Patricia Heimburg Torres",
+        "ci":4883039,
+        codigo:1,
+        "curso":"Curso Básico de HTML5"
+    },
+    {
+        "estudiante":"Matías Nicolás Bao Cassanello",
+        "ci":5403489,
+        "codigo":2,
+        "curso":"Curso Básico de HTML5"
+    },
+    {
+        "estudiante":"Guido Fernando Alcaraz Herebia",
+        "ci":2837909,
+        "codigo":3,
+        "curso":"Curso Básico de HTML5"
+    },
+    {
+        "estudiante":"Laura Cecilia Achón Gómez",
+        "ci":3965412,
+        "codigo":4,
+        "curso":"Curso Básico de HTML5"
+    },
+    {
+        "estudiante":"Mariel Raquel Aquino Villalba",
+        "ci":3494074,
+        "codigo":5,
+        "curso":"Curso Básico de HTML5"
+    },
+    {
+        "estudiante":"Giovanni Arzamendia",
+        "ci":2551888,
+        "codigo":6,
+        "curso":"Curso Básico de HTML5"
+    },
+    {
+        "estudiante":"Roberto Fleitas",
+        "ci":3553821,
+        "codigo":7,
+        "curso":"Curso Básico de HTML5"
+    },
+    {
+        "estudiante":"Nilda Natalia Rigoni Ibañez",
+        "ci":3831278,
+        "codigo":8,
+        "curso":"Curso Básico de HTML5"
+    },
+    {
+        "estudiante":"Jorge Gustavo Flecha Nuñez",
+        "ci":2353583,
+        "codigo":9,
+        "curso":"Curso Básico de HTML5"
+    },
+    {
+        "estudiante":"Alejandro Milciades Flores Vera",
+        "ci":5718095,
+        "codigo":10,
+        "curso":"Curso Básico de HTML5"
+    }
+    ];
